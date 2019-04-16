355f36a137f74c6da555116065e58619a0e1972d
Nolan Kuenzi <mr.nolank@gmail.com>
2019-04-16 00:27:33 -0700

    heroku config update

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 mongoose.Promise = global.Promise;
+
 mongoose.connect(process.env.DATABASE_URL);
 mongodb://localhost:27017/node-express-mongodb-server
 */
